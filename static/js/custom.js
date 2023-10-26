01c91a904fe95d5cf03a49ee2762cb4acf6d563c
Satori5ama <1242330740@qq.com>
2023-10-26 15:56:56 +0800

    update the comment area and the support for Latex

--- static/js/custom.js
+++ static/js/custom.js
@@ -71,3 +71,23 @@ jQuery(document).ready(function($) {
 		
     });
 });
+/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+window.onload = function() {
+	var OriginTitile = document.title;
+	var titleTime;
+	document.addEventListener('visibilitychange', function() {
+	  if(document.hidden) {
+		$('[rel="icon"]').attr('href', "/failure.ico");
+		$('[rel="shortcut icon"]').attr('href', "/failure.ico");
+		document.title = '哦~该回码头了';
+		clearTimeout(titleTime);
+	  } else {
+		$('[rel="icon"]').attr('href', "/favicon.svg");
+		$('[rel="shortcut icon"]').attr('href', "/favicon.svg");
+		document.title = '哈~又整到薯条了';
+		titleTime = setTimeout(function() {
+		  document.title = OriginTitile;
+		}, 2000);
+	  }
+	});
+  }
