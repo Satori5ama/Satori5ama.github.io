c0b996d0806c11f79579e0f93050f7d37e6a1f65
Satori5ama <1242330740@qq.com>
2025-01-03 09:24:27 +0800

    Fix some bugs.

--- static/live2d-widget/autoload.js
+++ static/live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://cdn.jsdmirror.net/gh/stevenjoezhang/live2d-widget@latest/";
-//const live2d_path = "/live2d-widget/";
+//const live2d_path = "https://cdn.jsdmirror.com/gh/stevenjoezhang/live2d-widget@latest/";
+const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "https://cdn.jsdmirror.com/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
